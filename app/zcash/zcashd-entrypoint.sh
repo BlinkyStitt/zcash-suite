2bd1ef01c3c10cc498d0b90153f2188e9bb7f3d7
Bryan Stitt <bryan@stitthappens.com>
2016-12-01 18:50:57 -0800

    improve start script

--- app/zcash/zcashd-entrypoint.sh
+++ app/zcash/zcashd-entrypoint.sh
@@ -3,16 +3,21 @@
 
 set -e
 
+# zcashd does not support name resolution for the proxy setting so we get the
+# IP for the Tor container now
+TOR_PROXY_IP=$(getent hosts tor | cut -f1 -d' ')
+if [ -z "$TOR_PROXY_IP" ]; then
+    echo "Cannot start zcashd until Tor is running."
+    exit 1
+fi
+
 cp /config/zcash.conf ~/.zcash/
 
 # TODO: add a note to the top of the copied config that they shouldn't touch it
 
-# zcashd does not support name resolution for the proxy setting so we get the
-# IP for the Tor container now
-TOR_PROXY_IP=$(getent hosts tor | cut -f1 -d' ')
 sed -i -e "/^proxy=/ s/=.*/=${TOR_PROXY_IP}:9050/" ~/.zcash/zcash.conf
 sed -i -e "/^onion=/ s/=.*/=${TOR_PROXY_IP}:9050/" ~/.zcash/zcash.conf
 
-zcash-fetch-params || exit 1
+zcash-fetch-params
 
 exec zcashd -printtoconsole "$@"
